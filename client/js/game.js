bd70680410b68fb8f94ac4921e56f426b9d11717
Matthew Farejowicz <mfarejow@mit.edu>
2020-01-07 13:35:05 -0500

    add basic shop

--- client/js/game.js
+++ client/js/game.js
@@ -1,6 +1,13 @@
 const GAME_WORLD_SIZE = 2048;
 const HALF_GAME_WORLD_SIZE = GAME_WORLD_SIZE / 2;
 const PLAYER_SPEED = 150;
+const SHOP_RANGE = 200;
+
+const distanceBetween = (r1, r2) => {
+  const dx = r1.x - r2.x;
+  const dy = r1.y - r2.y;
+  return Math.sqrt(Math.pow(dx, 2) + Math.pow(dy, 2));
+};
 class BootScene extends Phaser.Scene {
   constructor() {
     super({
@@ -14,6 +21,7 @@ class BootScene extends Phaser.Scene {
     this.load.image("player", "img/think.png");
     this.load.image("enemy", "img/angry.png");
     this.load.image("sword", "img/sword.png");
+    this.load.image("shop", "img/wink.png");
   }
 
   create() {
@@ -35,6 +43,7 @@ class WorldScene extends Phaser.Scene {
 
     this.createMap();
     this.createEnemies();
+    this.createShop();
     this.getInput();
 
     this.mouseMoved = false;
@@ -88,21 +97,20 @@ class WorldScene extends Phaser.Scene {
   }
 
   createPlayer(playerInfo) {
-    this.container = this.add.container(playerInfo.x, playerInfo.y);
-    this.physics.world.enable(this.container);
-    this.container.setSize(60, 60);
+    this.playerContainer = this.add.container(playerInfo.x, playerInfo.y);
+    this.physics.world.enable(this.playerContainer);
 
     // our player sprite created through the physics system
     this.player = this.add.sprite(0, 0, "player");
-    this.container.add(this.player);
+    this.playerContainer.setSize(this.player.width, this.player.height);
+    this.playerContainer.add(this.player);
 
     // add weapon
     this.weapon = this.add.sprite(0, -44, "sword");
-    this.weapon.setScale(0.2);
-    this.weapon.setSize(8, 8);
+    this.weapon.setScale(0.3);
     this.physics.world.enable(this.weapon);
 
-    this.container.add(this.weapon);
+    this.playerContainer.add(this.weapon);
     this.attacking = false;
     this.input.on("pointerdown", () => {
       if (!this.attacking) {
@@ -118,7 +126,7 @@ class WorldScene extends Phaser.Scene {
     this.updateCamera();
 
     // don't go out of the map
-    this.container.body.setCollideWorldBounds(true);
+    this.playerContainer.body.setCollideWorldBounds(true);
     // If you want collisions
     // this.physics.add.collider(this.container, this.spawns);
     this.physics.add.overlap(this.weapon, this.spawns, this.onMeetEnemy, false, this);
@@ -132,25 +140,25 @@ class WorldScene extends Phaser.Scene {
   }
 
   updateCamera() {
-    this.cameras.main.startFollow(this.container);
+    this.cameras.main.startFollow(this.playerContainer);
   }
 
   createOneEnemy(location) {
-    const container = this.add.container(location.x, location.y);
+    const enemyContainer = this.add.container(location.x, location.y);
 
-    const sprite = this.add.sprite(0, 0, "enemy");
-    container.setSize(sprite.width, sprite.height);
-    container.add(sprite);
+    const enemySprite = this.add.sprite(0, 0, "enemy");
+    enemyContainer.setSize(enemySprite.width, enemySprite.height);
+    enemyContainer.add(enemySprite);
 
-    container.health = 100;
+    enemyContainer.health = 100;
     const healthBackground = this.add.rectangle(0, -50, 100, 30, 0xff0000);
     healthBackground.name = "healthBackground";
-    container.add(healthBackground);
+    enemyContainer.add(healthBackground);
     const healthBar = this.add.rectangle(0, -50, 100, 30, 0x00ff00);
     healthBar.name = "healthBar";
-    container.add(healthBar);
+    enemyContainer.add(healthBar);
 
-    return container;
+    return enemyContainer;
   }
 
   createEnemies() {
@@ -252,32 +260,60 @@ class WorldScene extends Phaser.Scene {
     }
   }
 
+  createShop() {
+    this.add.circle(0, 0, SHOP_RANGE, 0x00ff00, 200);
+
+    this.shopContainer = this.add.container(0, 0);
+
+    // our player sprite created through the physics system
+    const shopSprite = this.add.sprite(0, 0, "shop");
+    this.shopContainer.add(shopSprite);
+
+    this.shopVisible = false;
+  }
+
+  handleShopToggle = () => {
+    const distanceToShop = distanceBetween(this.playerContainer, this.shopContainer);
+    if (distanceToShop < SHOP_RANGE && !this.shopVisible) {
+      this.scene.launch("ShopScene");
+      this.shopVisible = true;
+    } else {
+      this.scene.stop("ShopScene");
+      this.shopVisible = false;
+    }
+  };
+
   update() {
-    if (this.container) {
-      this.container.body.setVelocity(0);
+    if (this.playerContainer) {
+      this.playerContainer.body.setVelocity(0);
 
       // horizontal movement
-      if (this.buttons.left.isDown) {
-        this.container.body.setVelocityX(-PLAYER_SPEED);
-      } else if (this.buttons.right.isDown) {
-        this.container.body.setVelocityX(PLAYER_SPEED);
-      }
+      if (!this.shopVisible) {
+        if (this.buttons.left.isDown) {
+          this.playerContainer.body.setVelocityX(-PLAYER_SPEED);
+        } else if (this.buttons.right.isDown) {
+          this.playerContainer.body.setVelocityX(PLAYER_SPEED);
+        }
 
-      // vertical movement
-      if (this.buttons.up.isDown) {
-        this.container.body.setVelocityY(-PLAYER_SPEED);
-      } else if (this.buttons.down.isDown) {
-        this.container.body.setVelocityY(PLAYER_SPEED);
-      }
+        // vertical movement
+        if (this.buttons.up.isDown) {
+          this.playerContainer.body.setVelocityY(-PLAYER_SPEED);
+        } else if (this.buttons.down.isDown) {
+          this.playerContainer.body.setVelocityY(PLAYER_SPEED);
+        }
 
-      if (this.mouseMoved) {
-        this.pointer.updateWorldPoint(this.cameras.main);
-        const pointerPoint = {
-          x: this.pointer.worldX,
-          y: this.pointer.worldY,
-        };
-        const angleToPointer = Phaser.Math.Angle.BetweenPoints(this.container, pointerPoint);
-        this.container.rotation = angleToPointer + Math.PI / 2;
+        if (this.mouseMoved) {
+          this.pointer.updateWorldPoint(this.cameras.main);
+          const pointerPoint = {
+            x: this.pointer.worldX,
+            y: this.pointer.worldY,
+          };
+          const angleToPointer = Phaser.Math.Angle.BetweenPoints(
+            this.playerContainer,
+            pointerPoint
+          );
+          this.playerContainer.rotation = angleToPointer + Math.PI / 2;
+        }
       }
 
       if (this.attacking) {
@@ -285,18 +321,18 @@ class WorldScene extends Phaser.Scene {
       }
 
       // emit player movement
-      const x = this.container.x;
-      const y = this.container.y;
+      const x = this.playerContainer.x;
+      const y = this.playerContainer.y;
       if (
-        this.container.oldPosition &&
-        (x !== this.container.oldPosition.x || y !== this.container.oldPosition.y)
+        this.playerContainer.oldPosition &&
+        (x !== this.playerContainer.oldPosition.x || y !== this.playerContainer.oldPosition.y)
       ) {
         this.socket.emit("playerMovement", { x, y });
       }
       // save old position data
-      this.container.oldPosition = {
-        x: this.container.x,
-        y: this.container.y,
+      this.playerContainer.oldPosition = {
+        x: this.playerContainer.x,
+        y: this.playerContainer.y,
       };
     }
   }
@@ -307,8 +343,10 @@ class WorldScene extends Phaser.Scene {
       down: this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.S),
       left: this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.A),
       right: this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.D),
+      shop: this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.E),
     };
     this.pointer = this.input.mousePointer;
+    this.buttons.shop.on("down", this.handleShopToggle);
   }
 }
 
@@ -339,6 +377,19 @@ class HUDScene extends Phaser.Scene {
   }
 }
 
+class ShopScene extends Phaser.Scene {
+  constructor() {
+    super({ key: "ShopScene" });
+  }
+
+  create() {
+    const test = this.add.text(200, 200, "MENU!", {
+      font: "30px Arial",
+      fill: "#ffffff",
+    });
+  }
+}
+
 const gameConfig = {
   title: "upgrade",
   type: Phaser.AUTO,
@@ -353,7 +404,7 @@ const gameConfig = {
       debug: true,
     },
   },
-  scene: [BootScene, WorldScene, HUDScene],
+  scene: [BootScene, WorldScene, HUDScene, ShopScene],
 };
 
 const game = new Phaser.Game(gameConfig);
